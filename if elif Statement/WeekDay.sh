07f6eaaef45f46d549e54683595138341877d599
Akshay Wankhade <62167797+akshaywankhade@users.noreply.github.com>
2020-03-22 22:50:19 +0530

    if elif Statem

---
+++ if elif Statement/WeekDay.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -x
+
+read -p "Enter the Num:" day
+
+if [ $day -eq 1 ]
+then
+		printf "Sunday\n"
+elif [ $day -eq 2 ]
+then
+		printf "Monday\n"
+elif [ $day -eq 3 ]
+then
+		printf "Thesday\n"
+elif [ $day -eq 4 ]
+then
+		printf "Wednesday\n"
+elif [ $day -eq 5 ]
+then
+		printf "Thursday\n"
+elif [ $day -eq 6 ]
+then
+		printf "Friday\n"
+elif [ $day -eq 7 ]
+then
+		printf "Saturday\n"
+else
+		printf "Invalid input\n"
+fi
+
