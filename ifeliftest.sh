9456d64f694ad640fb0fb7c2f1820454f532e0b7
Akshay Wankhade <62167797+akshaywankhade@users.noreply.github.com>
2020-03-22 20:51:49 +0530

    IfelifStatement

---
+++ ifeliftest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+
+var1=10
+var2=10
+if [ $var2 -gt $var1 ]
+then
+	echo "$var2 is greater then $var1"
+elif [ $var2 -eq $var1 ]
+then
+	echo "Variables are equal"
+else
+	echo "$var2 is less then $var1"
+fi
