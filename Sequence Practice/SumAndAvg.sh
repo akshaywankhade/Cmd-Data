ec819ed3628de93fa3e4c8af1fe5e160db002580
Akshay Wankhade <62167797+akshaywankhade@users.noreply.github.com>
2020-03-26 18:23:02 +0530

    Delete SumAndAvg.sh

--- Sequence Practice/SumAndAvg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-Ran1=$(( 10+RANDOM%100 ))
-Ran2=$(( 10+RANDOM%100 ))
-Ran3=$(( 10+RANDOM%100 ))
-Ran4=$(( 10+RANDOM%100 ))
-Ran5=$(( 10+RANDOM%100 ))
-Sum=$(($Ran1+$Ran2+$Ran3+$Ran4+$Ran5))
-Avg=$(($Sum/5))
