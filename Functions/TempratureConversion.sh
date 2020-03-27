f79b96c986a78df6d41efe9d4f44cc4792ba6f8a
Akshay Wankhade <62167797+akshaywankhade@users.noreply.github.com>
2020-03-27 18:49:28 +0530

    ADDED: degF or degC conversion selection.

--- Functions/TempratureConversion.sh
+++ Functions/TempratureConversion.sh
@@ -4,11 +4,11 @@
 Choice1=1
 Choice2=2
 function celToFar(){
-		echo "$(( $(( $1*9 ))/5+32 ))"
+	echo "$(( $(( $1*9 ))/5+32 ))"
 }
 
 function farToCel(){
-		echo "$(( $(( $1-32 ))*5/9 ))"
+	echo "$(( $(( $1-32 ))*5/9 ))"
 }
 
 printf "Enter\n1 for degC to degF\n2 for degF to degC\n"
@@ -17,25 +17,24 @@ read -p "Enter Data: "data
 
 case $choice in
 		$Choice1)
-					if [[ $data -ge 0 && $data -le 100 ]]
-					then
-							result="$( celToFar $data )"
-					else
-							printf "Invalid Data"
-					fi
+			if [[ $data -ge 0 && $data -le 100 ]]
+			then
+				result="$( celToFar $data )"
+			else
+				printf "Invalid Data"
+			fi
 		;;
 		$Choice2)
-					if [[ $data -ge 32 && $data -le 212 ]]
-					then
-							result="$( farToCel $data)"
-					else
-							printf "Invalid Data"
-					fi
+			if [[ $data -ge 32 && $data -le 212 ]]
+			then
+				result="$( farToCel $data)"
+			else
+				printf "Invalid Data"
+			fi
 		;;
 		*)
 
-					printf "Invalid Choice"
+				printf "Invalid Choice"
 		;;
 esac
-
 printf "$result \n"
