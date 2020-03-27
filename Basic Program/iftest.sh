27ac24b74bfcdf6cf6f1b9063629abd78eaa3a97
Akshay Wankhade <62167797+akshaywankhade@users.noreply.github.com>
2020-03-27 18:18:33 +0530

    ADDED: Check if loop

--- Basic Program/iftest.sh
+++ Basic Program/iftest.sh
@@ -5,7 +5,7 @@ var1=10
 var2=10
 if [ $var1 -ge $var2 ]
 then
-		echo "$var2 is greater then or equal to $var1"
+	echo "$var2 is greater then or equal to $var1"
 else
-		echo "$var2 is less then $var1"
+	echo "$var2 is less then $var1"
 fi
