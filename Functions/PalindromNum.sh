05a7858b4db03812a90e44e3f4d2b7ad77395c95
Akshay Wankhade <62167797+akshaywankhade@users.noreply.github.com>
2020-03-27 18:37:37 +0530

    ADDED: Check the two number Palindrome.

--- Functions/PalindromNum.sh
+++ Functions/PalindromNum.sh
@@ -10,25 +10,25 @@ n1=$1
 n2=$2
 while [ $n1 -ne 0 ]
 do
-		rem=$(( $n1%10 ))
-		sum=$(( $sum*10+$rem ))
-		n1=$(( $n1/10 ))
+	rem=$(( $n1%10 ))
+	sum=$(( $sum*10+$rem ))
+	n1=$(( $n1/10 ))
 
 done
 
 if [ $sum -eq $n2 ]
 then
-		result=1
-		echo "$result"
+	result=1
+	echo "$result"
 else
-		echo "$result"
+	echo "$result"
 fi
 }
 
 result="$( isPalindrome $Num1 $Num2 )"
 if [ $result -eq 1 ]
 then
-		printf "Number is Palindrome\n"
+	printf "Number is Palindrome\n"
 else
-		printf "Number is Not Palindrome\n"
+	printf "Number is Not Palindrome\n"
 fi
